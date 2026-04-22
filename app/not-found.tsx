ea1fad3e3abd17cd57d882fbc949593a597885ca
Kostya Polishchuk <kostya.polishchuk@gmail.comgit congig --get user.namegit config --global user.email kostya.polishchuk@gmail.com>
2026-04-22 23:33:34 +0200

    init project

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+export default function NotFound() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "50vh",
+        textAlign: "center",
+      }}
+    >
+      <h1>404 - Page not found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+}
