5ec08c40fc3b64a803446f850ae872a332db439f
Denys Hedenko <unstoppa6le@gmail.com>
2026-04-25 23:05:44 +0200

    fix

--- app/(private)/profile/page.tsx
+++ app/(private)/profile/page.tsx
@@ -1,9 +1,28 @@
+"use client";
 import ProfileAvatar from "@/components/profile/ProfileAvatar/ProfileAvatar";
+import { getMe } from "@/lib/api/clientApi";
+import { useQuery } from "@tanstack/react-query";
 
 export default function ProfilePage() {
+  const {
+    data: user,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const user = await getMe();
+      return user;
+    },
+  });
+
+  if (isLoading) return <p>Завантаження...</p>;
+  if (isError || !user)
+    return <p>Сталася помилка. Спробуйте перегрузити сторінку</p>;
+
   return (
     <>
-      <ProfileAvatar />
+      <ProfileAvatar user={user} />
       <ProfileEditForm />
     </>
   );
