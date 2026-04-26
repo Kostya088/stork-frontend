72b2059e2b9c1b5b93edc4f31ffed14702e81dbc
Denys Hedenko <unstoppa6le@gmail.com>
2026-04-26 10:47:20 +0200

    done profileEditForm component

--- components/profile/ProfileEditForm/ProfileEditForm.tsx
+++ components/profile/ProfileEditForm/ProfileEditForm.tsx
@@ -5,18 +5,22 @@ import { User } from "@/types/user";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { ErrorMessage, Field, Form, Formik } from "formik";
 import toast from "react-hot-toast";
+import Select from "react-select";
 import * as yup from "yup";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import css from "./ProfileEditForm.module.css";
 
 const schemaUserForm = yup.object().shape({
   name: yup.string().required("Ім'я обов'язкове"),
-  gender: yup.string().oneOf(["boy", "girl", "null", ""], "Оберіть стать"),
+  gender: yup.string().oneOf(["boy", "girl", ""], "Оберіть стать"),
   dueDate: yup
     .date()
     .min(new Date(), "Дата має бути в майбутньому")
     .required("Дата обов'язкова"),
 });
 
-type GenderChildren = "boy" | "girl" | "null";
+type GenderChildren = "boy" | "girl" | "";
 
 interface ProfileEditFormProps {
   user: User | null;
@@ -30,7 +34,7 @@ interface FormDataValues {
 }
 
 const optionsForForm = [
-  { value: "null", label: "Ще не знаю" },
+  { value: "", label: "Ще не знаю" },
   { value: "boy", label: "Хлопчик" },
   { value: "girl", label: "Дівчинка" },
 ];
@@ -48,34 +52,111 @@ export default function ProfileEditForm({ user }: ProfileEditFormProps) {
   const handleSubmit = async (values: FormDataValues) => {
     mutation.mutate({
       name: values.name,
-      gender: values.gender === "null" ? "" : values.gender,
+      gender: values.gender === "" ? null : values.gender,
       dueDate: values.dueDate,
     });
   };
 
   const initialValues: FormDataValues = {
-    name:user?.name?? 'Дані не отримано',
-      email:user?.email ?? '',
-    gender:user?.gender ?? 'null'
-    dueDate: user?.dueDate ?? '',
+    name: user?.name ?? "Дані не отримано",
+    email: user?.email ?? "",
+    gender: user?.gender ?? "",
+    dueDate: user?.dueDate ?? "",
   };
 
   return (
-    <Formik <FormDataValues>
+    <Formik<FormDataValues>
       initialValues={initialValues}
       onSubmit={handleSubmit}
       validationSchema={schemaUserForm}
       enableReinitialize>
       {({ values, setFieldValue, resetForm, dirty }) => (
-        <Form className={css}>
-          <div className={css}>
-            <label htmlFor="">Імʼя</label>
-            <Field />
-            <ErrorMessage/>
+        <Form className={css.formWrapper}>
+          <div className={css.inputWrapper}>
+            <label htmlFor="name-Id" className={css.label}>
+              Імʼя
+            </label>
+            <Field type="text" name="name" id="name-Id" className={css.input} />
+            <ErrorMessage
+              name="name"
+              component="div"
+              className={css.errorMessage}
+            />
+          </div>
+
+          <div className={css.inputWrapper}>
+            <label htmlFor="email-Id" className={css.label}>
+              Пошта
+            </label>
+            <Field
+              type="email"
+              disabled
+              name="email"
+              id="email-Id"
+              className={css.input}
+            />
+          </div>
+
+          <div className={css.inputWrapper}>
+            <label htmlFor="gender-Id" className={css.label}>
+              {" "}
+              Оберіть стать
+            </label>
+            <Select
+              options={optionsForForm}
+              placeholder="Оберіть стать"
+              className={css.genderSelect}
+              classNamePrefix="gender"
+              value={optionsForForm.find(
+                (option) => option.value === values.gender,
+              )}
+              onChange={(optionsForForm) =>
+                setFieldValue("gender", optionsForForm?.value)
+              }
+            />
+          </div>
+
+          <div className={css.inputWrapper}>
+            <label htmlFor="dueDate-Id" className={css.label}>
+              Планована дата пологів
+            </label>
+            <DatePicker
+              selected={values.dueDate ? new Date(values.dueDate) : null}
+              onChange={(date: Date | null) =>
+                setFieldValue(
+                  "dueDate",
+                  date ? date.toISOString().split("T")[0] : "",
+                )
+              }
+              className={css.inputDate}
+              dateFormat="DD.MM.YYYY"
+              popperPlacement="top-start"
+              name="dueDate"
+              id="dueDate-Id"
+            />
+            <ErrorMessage
+              name="dueDate"
+              component="div"
+              className={css.errorMessage}
+            />
+          </div>
+
+          <div className={css.buttonWrapper}>
+            <button
+              type="button"
+              onClick={() => resetForm()}
+              className={css.buttonCancel}>
+              Відмінити зміни
+            </button>
+            <button
+              type="submit"
+              disabled={!dirty || mutation.isPending}
+              className={css.buttonSave}>
+              Зберігти зміни
+            </button>
           </div>
         </Form>
       )}
-      
     </Formik>
-  )
+  );
 }
