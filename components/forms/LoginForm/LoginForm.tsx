3d0903e0fdb3d2f70c0b89cd47085754ac036c04
seedre <taocripto@gmail.com>
2026-04-26 18:43:29 +0200

    add LoginForm

---
+++ components/forms/LoginForm/LoginForm.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/lib/store/authStore";
+import { login } from "@/lib/api/clientApi";
+import styles from "./LoginForm.module.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import Link from "next/link";
+import toast from "react-hot-toast";
+
+const schema = Yup.object({
+  email: Yup.string()
+    .email("Невірний email")
+    .required("Email обов'язковий"),
+  password: Yup.string().required("Пароль обов'язковий"),
+});
+
+export default function LoginForm() {
+  const router = useRouter();
+  const authStore = useAuthStore();
+//   const [error, setError] = useState("");
+
+  const handleSubmit = async (values: any, { setSubmitting }: any) => {
+    try {
+      const user = await login(values);
+
+      authStore.setUser(user);
+
+      toast.success("Успішний вхід");
+
+      router.push("/profile");
+    } catch (err) {
+      console.error(err);
+    //   setError("Невірний email або пароль");
+      toast.error("Помилка входу");
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Вхід</h2>
+
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validationSchema={schema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form className={styles.form}>
+
+            <div className={styles.field}>
+              <Field
+                name="email"
+                type="email"
+                placeholder="Пошта"
+                className={styles.input}
+              />
+              <ErrorMessage name="email" component="div" className={styles.error} />
+            </div>
+
+            <div className={styles.field}>
+              <Field
+                name="password"
+                type="password"
+                placeholder="Пароль"
+                className={styles.input}
+              />
+              <ErrorMessage name="password" component="div" className={styles.error} />
+            </div>
+
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className={styles.button}
+            >
+              {isSubmitting ? "Loading..." : "Увійти"}
+            </button>
+
+            <p className={styles.linkText}>
+              Немає акаунту?{" "}
+              <Link href="/auth/register" className={styles.link}>
+                Зареєструватись
+              </Link>
+            </p>
+
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+}
