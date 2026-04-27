f0f63280a472cb15919273511b7fdaf4c10d16aa
Denys Hedenko <unstoppa6le@gmail.com>
2026-04-27 15:42:44 +0200

    add all css for profilePage

--- components/profile/ProfileEditForm/ProfileEditForm.tsx
+++ components/profile/ProfileEditForm/ProfileEditForm.tsx
@@ -66,97 +66,104 @@ export default function ProfileEditForm({ user }: ProfileEditFormProps) {
   };
 
   return (
-    <Formik<FormDataValues>
-      initialValues={initialValues}
-      onSubmit={handleSubmit}
-      validationSchema={schemaUserForm}
-      enableReinitialize>
-      {({ values, setFieldValue, resetForm, dirty }) => (
-        <Form className={css.formWrapper}>
-          <div className={css.inputWrapper}>
-            <label htmlFor="name-Id" className={css.label}>
-              Імʼя
-            </label>
-            <Field type="text" name="name" id="name-Id" className={css.input} />
-            <ErrorMessage
-              name="name"
-              component="div"
-              className={css.errorMessage}
-            />
-          </div>
+    <div className={css.mainWrapper}>
+      <Formik<FormDataValues>
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={schemaUserForm}
+        enableReinitialize>
+        {({ values, setFieldValue, resetForm, dirty }) => (
+          <Form className={css.formWrapper}>
+            <div className={css.inputWrapper}>
+              <label htmlFor="name-Id" className={css.label}>
+                Імʼя
+              </label>
+              <Field
+                type="text"
+                name="name"
+                id="name-Id"
+                className={css.input}
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className={css.errorMessage}
+              />
+            </div>
 
-          <div className={`${css.inputWrapper} ${css.email}`}>
-            <label htmlFor="email-Id" className={css.label}>
-              Пошта
-            </label>
-            <Field
-              type="email"
-              disabled
-              name="email"
-              id="email-Id"
-              className={css.input}
-            />
-          </div>
+            <div className={`${css.inputWrapper} ${css.email}`}>
+              <label htmlFor="email-Id" className={css.label}>
+                Пошта
+              </label>
+              <Field
+                type="email"
+                disabled
+                name="email"
+                id="email-Id"
+                className={css.input}
+              />
+            </div>
 
-          <div className={css.inputWrapper}>
-            <label htmlFor="gender-Id" className={css.label}>
-              Стать дитини
-            </label>
-            <Select
-              options={optionsForForm}
-              placeholder="Стать дитини"
-              className={css.genderSelect}
-              classNamePrefix="gender"
-              value={optionsForForm.find(
-                (option) => option.value === values.gender,
-              )}
-              onChange={(optionsForForm) =>
-                setFieldValue("gender", optionsForForm?.value)
-              }
-            />
-          </div>
+            <div className={css.inputWrapper}>
+              <label htmlFor="gender-Id" className={css.label}>
+                Стать дитини
+              </label>
+              <Select
+                options={optionsForForm}
+                placeholder="Стать дитини"
+                className={css.genderSelect}
+                classNamePrefix="gender"
+                value={optionsForForm.find(
+                  (option) => option.value === values.gender,
+                )}
+                onChange={(optionsForForm) =>
+                  setFieldValue("gender", optionsForForm?.value)
+                }
+              />
+            </div>
 
-          <div className={css.inputWrapper}>
-            <label htmlFor="dueDate-Id" className={css.label}>
-              Планована дата пологів
-            </label>
-            <DatePicker
-              selected={values.dueDate ? new Date(values.dueDate) : null}
-              onChange={(date: Date | null) =>
-                setFieldValue(
-                  "dueDate",
-                  date ? date.toISOString().split("T")[0] : "",
-                )
-              }
-              className={css.inputDate}
-              dateFormat="dd.MM.yyyy"
-              popperPlacement="top-start"
-              name="dueDate"
-              id="dueDate-Id"
-            />
-            <ErrorMessage
-              name="dueDate"
-              component="div"
-              className={css.errorMessage}
-            />
-          </div>
+            <div className={css.inputWrapper}>
+              <label htmlFor="dueDate-Id" className={css.label}>
+                Планована дата пологів
+              </label>
+              <DatePicker
+                selected={values.dueDate ? new Date(values.dueDate) : null}
+                onChange={(date: Date | null) =>
+                  setFieldValue(
+                    "dueDate",
+                    date ? date.toISOString().split("T")[0] : "",
+                  )
+                }
+                className={css.inputDate}
+                dateFormat="dd.MM.yyyy"
+                popperPlacement="top-start"
+                name="dueDate"
+                id="dueDate-Id"
+              />
+              <ErrorMessage
+                name="dueDate"
+                component="div"
+                className={css.errorMessage}
+              />
+            </div>
 
-          <div className={css.buttonWrapper}>
-            <button
-              type="button"
-              onClick={() => resetForm()}
-              className={css.buttonCancel}>
-              Відмінити зміни
-            </button>
-            <button
-              type="submit"
-              disabled={!dirty || mutation.isPending}
-              className={css.buttonSave}>
-              Зберігти зміни
-            </button>
-          </div>
-        </Form>
-      )}
-    </Formik>
+            <div className={css.buttonWrapper}>
+              <button
+                type="button"
+                onClick={() => resetForm()}
+                className={css.buttonCancel}>
+                Відмінити зміни
+              </button>
+              <button
+                type="submit"
+                disabled={!dirty || mutation.isPending}
+                className={css.buttonSave}>
+                Зберігти зміни
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
