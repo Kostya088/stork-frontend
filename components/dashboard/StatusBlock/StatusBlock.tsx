968b12e25a9d2733891fd340b1ce353e5896eae5
Kateryna Havryna <khavryna@gmail.com>
2026-04-28 17:20:33 +0200

    fix

--- components/dashboard/StatusBlock/StatusBlock.tsx
+++ components/dashboard/StatusBlock/StatusBlock.tsx
@@ -1,3 +1,57 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useAuthStore } from "@/lib/store/authStore";
+import { getWeeksMe } from "@/lib/api/clientApi";
+
+import css from "./StatusBlock.module.css";
+
 export default function StatusBlock() {
-  return null;
+  const isAuthenticated = useAuthStore(
+    (s) => s.isAuthenticated,
+  );
+
+  const { data, isLoading } = useQuery({
+    queryKey: ["weeksMe", isAuthenticated],
+    queryFn: getWeeksMe,
+    enabled: isAuthenticated,
+  });
+
+  if (!isAuthenticated || isLoading || !data) {
+    return (
+      <div className={css.row}>
+        <div className={css.card}>
+          <p className={css.label}>Тиждень</p>
+          <p className={css.value}>—</p>
+        </div>
+
+        <div className={css.card}>
+          <p className={css.label}>
+            Днів до зустрічі
+          </p>
+          <p className={css.value}>—</p>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={css.row}>
+      <div className={css.card}>
+        <p className={css.label}>Тиждень</p>
+        <p className={css.value}>
+          {data?.weekNumber}
+        </p>
+      </div>
+
+      <div className={css.card}>
+        <p className={css.label}>
+          Днів до зустрічі
+        </p>
+        <p className={css.value}>
+          {data?.daysUntilDue}
+        </p>
+      </div>
+    </div>
+  );
 }
