ebd8d4ae8ba38b5e1b611a18f09f8adbe0de5d3c
Denys Ovcharov <denisovcharov88@gmail.com>
2026-04-28 21:22:47 +0200

    denis-push

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Login() {
-   return (
+  return (
     <div className={styles.page}>
       <Link href="/" className={styles.logoLink}>
         <svg className={styles.logoIcon} width="95" height="29">
