146658e25187a9c3ca268b585f162bcda329b1c6
Denys Ovcharov <denisovcharov88@gmail.com>
2026-04-28 22:58:08 +0200

    fix-sidebar-on-edit-page

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,17 +1,27 @@
+"use client";
+
 import Breadcrumbs from "@/components/layout/Breadcrumbs/Breadcrumbs";
 import { DesktopSidebar } from "@/components/layout/SideBar/SideBar";
 import css from "./layout.module.css";
+import { usePathname } from "next/navigation";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export default function PrivateLayout({ children }: Props) {
+  const pathname = usePathname();
+  const isAuthPage =
+    pathname === "/login" ||
+    pathname === "/register" ||
+    pathname === "/profile/edit";
   return (
-    <div className={css.layout}>
-      <aside className={css.sidebarDesktop}>
-        <DesktopSidebar />
-      </aside>
+    <div className={`${css.layout} ${isAuthPage ? css.noSidebar : ""}`}>
+      {!isAuthPage && (
+        <aside className={css.sidebarDesktop}>
+          <DesktopSidebar />
+        </aside>
+      )}
       <div className={css.main}>
         <Breadcrumbs />
         {children}
