2f4e86b42fc656f18042295d6a6fe1f85f5eeb2d
Voloshyn9900 <voloshyn9900@gmail.com>
2026-04-29 22:10:47 +0300

    first

--- app/(private)/journey/[weekNumber]/page.tsx
+++ app/(private)/journey/[weekNumber]/page.tsx
@@ -1,3 +1,23 @@
-export default function JourneyDetailsPage() {
-  return <p>JourneyPage</p>;
+import GreetingBlock from "@/components/dashboard/GreetingBlock/GreetingBlock";
+import WeekSelector from "@/components/journey/WeekSelector/WeekSelector";
+import JourneyDetails from "@/components/journey/JourneyDetails/JourneyDetails";
+// import styles from "./page.module.css";
+
+interface Props {
+  params: Promise<{ weekNumber: string }>;
+}
+
+export default async function JourneyDetailsPage({ params }: Props) {
+  const { weekNumber } = await params;
+  const week = Number(weekNumber);
+
+  return (
+    // <div className={styles.page}>
+    <div>
+      <GreetingBlock />
+
+      <WeekSelector selectedWeek={week} />
+      <JourneyDetails weekNumber={week} />
+    </div>
+  );
 }
