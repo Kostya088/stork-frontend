90440d49ce5678c96900106c596c6cb3a1abd92d
Max <yurchenko.maksym@gmail.com>
2026-05-07 14:50:59 +0100

    fix google auth integration

--- app/api/api.ts
+++ app/api/api.ts
@@ -3,6 +3,6 @@ import axios, { AxiosError } from 'axios';
 export type ApiError = AxiosError<{ error: string }>;
 
 export const api = axios.create({
-  baseURL: 'https://stork-backend.onrender.com',
+  baseURL: process.env.BACKEND_API_URL ??'https://stork-backend.onrender.com',
   withCredentials: true,
 });
--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,9 @@ const nextConfig: NextConfig = {
     protocol: "https",
     hostname: "res.cloudinary.com",
   },
+    {protocol: 'https',
+    hostname: 'lh3.googleusercontent.com',
+  }
 ],
   },
 };
